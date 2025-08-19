14a95d008b8ba7da26a66b14466ed729a7dd829b
pawan kumar gond <98225403+kpawangond@users.noreply.github.com>
2025-08-20 01:13:59 +0530

    Update wsgi.py

--- TheSkillTree/TheSkillsTree/TheSkillsTree/wsgi.py
+++ TheSkillTree/TheSkillsTree/TheSkillsTree/wsgi.py
@@ -11,7 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheSkillsTree.settings')
+"""os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheSkillsTree.settings')"""
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheSkillTree.TheSkillsTree.settings')
+
 """os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheSkillTree.TheSkillsTree.settings')"""
 
 
